f33383b7ad0723622d5019e60b4e63d7bc9879fc
marinoscar <oscar@marin.cr>
2020-03-02 02:37:02 -0600

    initial commit

---
+++ Code/luval.taxes.app/MergeExcelFiles.cs
@@ -0,0 +1,48 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace luval.taxes.app
+{
+    public class MergeExcelFiles
+    {
+        public static void DoMerge(IEnumerable<FileInfo> files, FileInfo output, int startColumn, int endColumn)
+        {
+            if (output.Exists) output.Delete();
+            using (var outputPackage = new ExcelPackage(output))
+            {
+                using (var outputSheet = outputPackage.Workbook.Worksheets.Add("output"))
+                {
+                    var outputRowCount = 1;
+                    foreach (var inputFile in files)
+                    {
+                        if (!inputFile.Exists) continue;
+                        using (var excelPackage = new ExcelPackage(inputFile))
+                        {
+                            var rowCount = files.First() == inputFile ? 1 : 2;
+                            using (var sheet = excelPackage.Workbook.Worksheets.First())
+                            {
+                                var isEof = string.IsNullOrWhiteSpace(Convert.ToString(sheet.Cells[rowCount, startColumn].Value));
+                                while (!isEof)
+                                {
+                                    for (int i = startColumn; i < endColumn; i++)
+                                    {
+                                        outputSheet.Cells[outputRowCount, i].Value = sheet.Cells[rowCount, i].Value;
+                                    }
+                                    rowCount++;
+                                    outputRowCount++;
+                                    isEof = string.IsNullOrWhiteSpace(Convert.ToString(sheet.Cells[rowCount, startColumn].Value));
+                                }
+                            }
+                        }
+                    }
+                    outputPackage.Save();
+                }
+            }
+        }
+    }
+}
---
+++ Code/luval.taxes.app/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace luval.taxes.app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dir = new DirectoryInfo(@"C:\Users\ch489gt\Google Drive\Work\EY\Personal-Taxes-2019\Timesheets");
+            var files = dir.GetFiles("*.xls*", SearchOption.AllDirectories)
+                .Where(i => i.Name != "result.xlsx" && !i.Name.StartsWith("~")).ToList();
+            MergeExcelFiles.DoMerge(files, new FileInfo(Path.Combine(dir.FullName, "result.xlsx")), 1, 35);
+        }
+    }
+}
